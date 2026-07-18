bc774eca160971e5e90f31bec8c0203a20d4de15
otzhimaniya <otzhimaniya@users.noreply.github.com>
2026-07-18 10:43:18 +0300

    Modernize app design and trainer experience

--- android/app/src/main/assets/public/sw.js
+++ android/app/src/main/assets/public/sw.js
@@ -1,5 +1,5 @@
-const CACHE='pushup-v12';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./vendor/fonts/fonts.css'];
+const CACHE='pushup-v20';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./vendor/fonts/fonts.css','./assets/coach-layma-fox-v1.png','./assets/coach-lime-fox-v1.png','./assets/hero-man-pushup-v1.png','./assets/technique-gym-bg-v1.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
--- android/app/src/main/java/com/otzhimaniya/app/MainActivity.java
+++ android/app/src/main/java/com/otzhimaniya/app/MainActivity.java
@@ -1,5 +1,23 @@
 package com.otzhimaniya.app;
 
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().setNavigationBarColor(Color.BLACK);
+        WindowInsetsControllerCompat controller = WindowCompat.getInsetsController(
+                getWindow(), getWindow().getDecorView());
+        controller.setAppearanceLightStatusBars(false);
+        controller.setAppearanceLightNavigationBars(false);
+    }
+}
